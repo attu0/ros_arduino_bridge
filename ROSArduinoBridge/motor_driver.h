dcaef1dcc8a25cfcabcf854f50cb4a0215845065
attu0 <atharvmudse@gmail.com>
2026-02-18 23:21:39 +0530

    added my working code for articubot 1

--- ROSArduinoBridge/motor_driver.h
+++ ROSArduinoBridge/motor_driver.h
@@ -3,12 +3,18 @@
    *************************************************************/
 
 #ifdef L298_MOTOR_DRIVER
-  #define RIGHT_MOTOR_BACKWARD 5
-  #define LEFT_MOTOR_BACKWARD  6
-  #define RIGHT_MOTOR_FORWARD  9
-  #define LEFT_MOTOR_FORWARD   10
-  #define RIGHT_MOTOR_ENABLE 12
-  #define LEFT_MOTOR_ENABLE 13
+  // #define RIGHT_MOTOR_BACKWARD 5
+  // #define LEFT_MOTOR_BACKWARD  6
+  // #define RIGHT_MOTOR_FORWARD  9
+  // #define LEFT_MOTOR_FORWARD   10
+  // #define RIGHT_MOTOR_ENABLE 12
+  // #define LEFT_MOTOR_ENABLE 13
+  #define RIGHT_MOTOR_ENABLE 5
+  #define LEFT_MOTOR_ENABLE 6
+  #define RIGHT_MOTOR_FORWARD 9
+  #define RIGHT_MOTOR_BACKWARD 8
+  #define LEFT_MOTOR_FORWARD 10
+  #define LEFT_MOTOR_BACKWARD 11
 #endif
 
 void initMotorController();
